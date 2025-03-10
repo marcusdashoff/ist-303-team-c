09f3fc2cdbf65206a3b938db47681664e2439e13
Chen Zhu <patrickzhu1130@gmail.com>
2025-03-10 15:55:52 -0700

    Move search to its own controller

---
+++ controllers/search.py
@@ -0,0 +1,35 @@
+from flask import Blueprint, render_template, request, redirect, url_for
+from helper.db_connector import get_db_connection
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+
+search_controller = Blueprint('search', __name__)
+
+@search_controller.route('/search', methods=['GET', 'POST'])
+def search():
+    if not (current_user and current_user.is_authenticated):
+        return redirect(url_for('auth.login'))
+
+    if request.method == 'POST':
+        ticker = request.form['ticker']
+        conn = get_db_connection()
+        stock = conn.execute('SELECT * FROM stocks WHERE ticker = ?', (ticker,)).fetchone()
+        current_price = conn.execute('SELECT price FROM sells INNER JOIN stocks ON (stocks.id = stock_id) WHERE ticker = ? ORDER BY datetime DESC LIMIT 1', (ticker,)).fetchone()
+        historical_high = conn.execute('SELECT MAX(price) as price FROM sells INNER JOIN stocks ON (stocks.id = stock_id) WHERE ticker = ?', (ticker,)).fetchone()
+        historical_low = conn.execute('SELECT MIN(price) as price FROM sells INNER JOIN stocks ON (stocks.id = stock_id) WHERE ticker = ?', (ticker,)).fetchone()
+        trading_volume_with_past_24_hours = conn.execute(
+            'SELECT SUM(price) as price FROM sells INNER JOIN stocks ON (stocks.id = stock_id) WHERE ticker = ? AND datetime >= datetime("now", "-24 hours")',
+            (ticker,)
+        ).fetchone()
+        if stock:
+            return render_template(
+                'search.html',
+                user=current_user,
+                stock=stock,
+                current_price=current_price,
+                historical_high=historical_high,
+                historical_low=historical_low,
+                trading_volume_with_past_24_hours=trading_volume_with_past_24_hours
+            )
+        else:
+            return render_template('search.html', user=current_user, error='Woow I What Ya Tryna Search')
+    return render_template('search.html', user=current_user)
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template, redirect, url_for, request, abort, flash
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 from models.user import User
+from controllers.search import search_controller
+from helper.db_connector import get_db_connection
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'abcdefghijklmnop'
 login_manager = LoginManager()
 login_manager.init_app(app)
+app.register_blueprint(search_controller)
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -39,32 +42,4 @@ def logout():
     if current_user and current_user.is_authenticated:
         logout_user()
         return redirect(url_for('login'))
-    
-@app.route('/search', methods=('GET', 'POST'))
-def search():
-    if not (current_user and current_user.is_authenticated):
-        return redirect(url_for('login'))
-    if request.method == 'POST':
-        ticker = request.form['ticker']
-        conn = get_db_connection()
-        stock = conn.execute('SELECT * FROM stocks WHERE ticker = ?', (ticker,)).fetchone()
-        current_price = conn.execute('SELECT price FROM sells INNER JOIN stocks ON (stocks.id = stock_id) WHERE ticker = ? ORDER BY datetime DESC LIMIT 1', (ticker,)).fetchone()
-        historical_high = conn.execute('SELECT MAX(price) as price FROM sells INNER JOIN stocks ON (stocks.id = stock_id) WHERE ticker = ?', (ticker,)).fetchone()
-        historical_low = conn.execute('SELECT MIN(price) as price FROM sells INNER JOIN stocks ON (stocks.id = stock_id) WHERE ticker = ?', (ticker,)).fetchone()
-        trading_volume_with_past_24_hours = conn.execute(
-            'SELECT SUM(price) as price FROM sells INNER JOIN stocks ON (stocks.id = stock_id) WHERE ticker = ? AND datetime >= datetime("now", "-24 hours")', 
-            (ticker,)).fetchone()
-        
-        if stock:
-            return render_template('search.html', 
-                                   user=current_user, 
-                                   stock=stock, 
-                                   current_price=current_price, 
-                                   historical_high=historical_high, 
-                                   historical_low=historical_low,
-                                   trading_volume_with_past_24_hours=trading_volume_with_past_24_hours
-                                   )
-        else:
-            return render_template('search.html', user=current_user, error='Woow I What Ya Tryna Search')
-    return render_template('search.html', user=current_user)
     
